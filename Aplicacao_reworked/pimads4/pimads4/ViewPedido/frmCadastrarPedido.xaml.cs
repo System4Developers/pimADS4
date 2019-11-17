6bd61de18dbedb503b202d045d5abe5cae9ed25e
njunior2593 <neelson_jr@live.com>
2019-11-17 15:12:48 -0300

    ajusts no form do pedido

--- Aplicacao_reworked/pimads4/pimads4/ViewPedido/frmCadastrarPedido.xaml.cs
+++ Aplicacao_reworked/pimads4/pimads4/ViewPedido/frmCadastrarPedido.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pimads4.ViewProduto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,6 +31,26 @@ namespace pimads4.frmPedido
         private void BtnClosePedido_Click(object sender, RoutedEventArgs e)
         {
          
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnAddProduto_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "Teste Tela1",
+                Height = 620,
+                Width = 620,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                WindowStyle = WindowStyle.ToolWindow,
+                Content = new frmManterProdutos()
+            };
+            window.Show();
+
         }
     }
 }
